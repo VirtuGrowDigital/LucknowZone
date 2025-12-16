248ee5a5cc12858f5a4c7cb49359df46325ee8a9
ARSNiC13 <animeshyadav70@gmail.com>
2025-12-16 16:55:38 +0530

    SaveLogic

--- NewsBackend/src/routes/savedRoutes.js
+++ NewsBackend/src/routes/savedRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import { saveArticle, getSavedArticles } from "../controllers/savedController.js";
+import { saveArticle, getSavedArticles, deleteSavedArticle } from "../controllers/savedController.js";
 
 const router = express.Router();
 
