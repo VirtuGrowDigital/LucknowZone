2cbef2ef211abeb27319b4512a306dc4b2c57e2b
ARSNiC13 <animeshyadav70@gmail.com>
2025-12-16 14:27:48 +0530

    fix express options wildcard crash

--- NewsFrontend/src/App.jsx
+++ NewsFrontend/src/App.jsx
@@ -1,41 +1,47 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+import MainNav from "./Components/MainNav";
+
+import TopStories from "./pages/TopStories";
+import CategoryPage from "./pages/CategoryPage";
+
+import { WeatherProvider } from "./context/WeatherContext";
+import Footer from "./Components/Footer";
+import BlogPage from "./pages/BlogPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
+
+
+
+
 export default function App() {
   return (
     <WeatherProvider>
-      {/* PAGE LAYOUT */}
-      <div className="min-h-screen flex flex-col">
-        {/* TOP */}
-        <Header />
-        <MainNav />
+      <Header />
+      <HeroSection />
+      <MainNav />
 
-        {/* MAIN CONTENT */}
-        <main className="flex-1">
-          <HeroSection />
+      <Routes>
+        {/* REAL HOME */}
+        <Route path="/" element={<TopStories />} />
 
-          <Routes>
-            {/* HOME */}
-            <Route path="/" element={<TopStories />} />
+        {/* Top Stories tab maps to homepage */}
+        <Route path="/top-stories" element={<Navigate to="/" />} />
+        <Route path="/blog/:slug" element={<BlogDetailPage />} />
 
-            {/* Top Stories alias */}
-            <Route path="/top-stories" element={<Navigate to="/" />} />
-
-            {/* BLOG */}
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/:slug" element={<BlogDetailPage />} />
-
-            {/* CATEGORY PAGES */}
-            <Route path="/local" element={<CategoryPage />} />
-            <Route path="/politics" element={<CategoryPage />} />
-            <Route path="/sports" element={<CategoryPage />} />
-            <Route path="/tech" element={<CategoryPage />} />
-            <Route path="/health" element={<CategoryPage />} />
-            <Route path="/business" element={<CategoryPage />} />
-            <Route path="/entertainment" element={<CategoryPage />} />
-          </Routes>
-        </main>
-
-        {/* FOOTER */}
-        <Footer />
-      </div>
+        {/* Category routes */}
+        <Route path="/local" element={<CategoryPage />} />
+        <Route path="/politics" element={<CategoryPage />} />
+        <Route path="/sports" element={<CategoryPage />} />
+        <Route path="/tech" element={<CategoryPage />} />
+        <Route path="/health" element={<CategoryPage />} />
+        <Route path="/business" element={<CategoryPage />} />
+        <Route path="/entertainment" element={<CategoryPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+      </Routes>
+      <Footer />
     </WeatherProvider>
+    
   );
 }
